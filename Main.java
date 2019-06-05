5d43c53323ffa2859494c10ea9dda242a70e8b40
jokevandermik <44468502+jokevandermik@users.noreply.github.com>
2019-06-05 14:36:27 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,63 +1,87 @@
-package p3;
+package hu.nl.hibernate;
 
-import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;  
+import hu.nl.hibernate.dao.OVChipkaartService;
+import hu.nl.hibernate.dao.ReizigerService;
 
+import java.sql.Date;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+
+@SuppressWarnings("ConstantConditions")
 public class Main {
-	public static void main(String[] args) throws ParseException {
-		
-		
-		ReizigerOracleDaoImpl db = new ReizigerOracleDaoImpl();
-		OVChipkaartDaoImpl dbov = new OVChipkaartDaoImpl();
-		ProductDaoImpl dbp = new ProductDaoImpl();
 
-		Reiziger reiziger = new Reiziger();
-		
-		reiziger.setVoorletter("J");
-		reiziger.setAchternaam("van der Mik");
-		
-		
-		Date datum1 = new SimpleDateFormat("dd/MM/yyyy").parse("17/04/2019");
-		
-		reiziger.setGBdatum(datum1);
+    public static void main(String[] args) {
+        OVChipkaart ovChipkaart = OVChipkaartService.getInstance().findById(35283);
+        ConsoleColor.log("---- Get OVChipkaart (KAARTNUMMER=35283)", ConsoleColor.ANSI_BLUE);
 
-		db.save(reiziger);
-		
-		OVChipkaart ov = new OVChipkaart();
-		
-		ov.setKaartNummer(1234567);
-		ov.setKlasse(1);
-		ov.setId(2);
-		ov.setSaldo(234.43);
-		dbov.save(ov);
-		
-		for (Reiziger r : db.findAllByGBdatum("03-12-2002")) {
-			System.out.println(r.getVoorletter() + " " + r.getAchternaam());
-		}
-		
-		for (OVChipkaart ovv : dbov.findByReiziger(2)) {
-			System.out.println(ovv.getKaartNummer());
-		}
-		
-		Product product1 = new Product();
-		product1.setProductNaam("2test3");
-		product1.setBeschrijving("Dit is de beschrijving 1");
-		product1.setPrijs(50.2);
-		product1.setProductNummer(14);
-		dbp.save(product1);
-		
-		Product product2 = new Product();
-		product2.setProductNaam("1test4");
-		product2.setBeschrijving("Dit is de beschrijving 2");
-		product2.setPrijs(10.3);
-		product2.setProductNummer(15);
-		dbp.save(product2);
-		
-		dbov.linkProduct(ov, product1);
+        ConsoleColor.log(ovChipkaart, ConsoleColor.ANSI_CYAN);
+        ConsoleColor.log(ovChipkaart.getReiziger(), ConsoleColor.ANSI_CYAN);
 
-		for (OVChipkaart ovv : dbov.findByReiziger(2)) {
-			System.out.println(ovv.getKaartNummer());
-		}
-	}
-} 
+
+        ConsoleColor.log("\n---- Get Reiziger (ID=5)", ConsoleColor.ANSI_BLUE);
+        ReizigerService.getInstance().getReizigerDao().openCurrentSession();
+        Reiziger reiziger = ReizigerService.getInstance().getReizigerDao().findById(5);
+        ConsoleColor.log(reiziger, ConsoleColor.ANSI_CYAN);
+
+        ConsoleColor.log("\n---- Get Reiziger's OVChipkaarten (same instance as previous)", ConsoleColor.ANSI_BLUE);
+        reiziger.getOVChipkaarten().forEach(r -> ConsoleColor.log(r, ConsoleColor.ANSI_CYAN));
+
+
+        ConsoleColor.log("\n---- Update Reiziger (same instance as previous; achternaam=TEST)", ConsoleColor.ANSI_BLUE);
+        reiziger.setAchternaam("TEST");
+        ReizigerService.getInstance().getReizigerDao().update(reiziger);
+        ReizigerService.getInstance().getReizigerDao().closeCurrentSession();
+
+        // Load object from database
+        reiziger = ReizigerService.getInstance().findById(5);
+        ConsoleColor.log(reiziger, ConsoleColor.ANSI_CYAN);
+
+        // Revert back
+        reiziger.setAchternaam("Piccardo");
+        ReizigerService.getInstance().update(reiziger);
+
+        if (true) {
+            ConsoleColor.log("\n---- Create Reiziger with OVChipkaart", ConsoleColor.ANSI_BLUE);
+            reiziger = new Reiziger(6, "Bart", "den", "Hamer",
+                    Date.valueOf(new GregorianCalendar(2000, Calendar.FEBRUARY, 10)
+                            .toInstant()
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDate()));
+
+            ovChipkaart = new OVChipkaart(12345,
+                    Date.valueOf(new GregorianCalendar(2019, Calendar.DECEMBER, 10)
+                            .toInstant()
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDate()),
+                    2, 50f, reiziger);
+            reiziger.setOVChipkaarten(new ArrayList<>(Collections.singletonList(ovChipkaart)));
+
+            ReizigerService.getInstance().persist(reiziger);
+
+            reiziger = ReizigerService.getInstance().findById(6);
+            ConsoleColor.log(reiziger, ConsoleColor.ANSI_CYAN);
+        }
+
+
+        if (true) {
+            ConsoleColor.log("\n---- Delete Reiziger", ConsoleColor.ANSI_BLUE);
+
+            ReizigerService.getInstance().delete(reiziger);
+
+            reiziger = ReizigerService.getInstance().findById(6);
+
+            if (reiziger == null) {
+                ConsoleColor.log("Reiziger verwijderd", ConsoleColor.ANSI_CYAN);
+            } else {
+                ConsoleColor.log("Reiziger NIET verwijderd", ConsoleColor.ANSI_CYAN);
+            }
+        }
+
+        ConsoleColor.log("\n---- Shutdown", ConsoleColor.ANSI_BLUE);
+        HibernateUtil.shutdown();
+    }
+
+}
